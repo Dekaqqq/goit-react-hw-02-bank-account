f6b4402a9db80060bbded222e32edd306ee1d33c
dekaqqq <kdrushlyak@gmail.com>
2019-09-24 17:48:18 +0300

    Updates

---
+++ precache-manifest.853f005323c5e8a4c269c78d696e951e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5964b42516e6394b714ea4c4b4e70",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "62d8357e6c81760e412c",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "9b39c602c25dd7e8e6e9",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/static/css/main.5a0274c2.chunk.css"
+  },
+  {
+    "revision": "62d8357e6c81760e412c",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/static/js/2.5f5ba96c.chunk.js"
+  },
+  {
+    "revision": "9b39c602c25dd7e8e6e9",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/static/js/main.1273d6b5.chunk.js"
+  },
+  {
+    "revision": "75607234ba6a0c7fe606",
+    "url": "/Dekaqqq/goit-react-hw-02-bank-account/static/js/runtime~main.128489a6.js"
+  }
+]);
